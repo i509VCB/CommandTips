e9ebd90bb666d441a5b1174865e9161aedc601ca
i509vcb <git@i509.me>
2020-03-08 01:08:51 -0600

    Make the scrolling of suggestions work with the item/block/player head popups

--- src/main/java/me/i509/fabric/commandtips/mixin/screen/CommandSuggestor_SuggestionWindowMixin_Core.java
+++ src/main/java/me/i509/fabric/commandtips/mixin/screen/CommandSuggestor_SuggestionWindowMixin_Core.java
@@ -15,6 +15,7 @@ import net.minecraft.client.util.math.Vector3f;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.SignItem;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -27,48 +28,52 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 public abstract class CommandSuggestor_SuggestionWindowMixin_Core {
 	@Shadow private Rect2i area;
 	@Shadow @Final private Suggestions suggestions;
+	@SuppressWarnings("ShadowTarget") @Shadow private CommandSuggestor field_21615;
+
+	@Shadow private int inWindowIndex;
 
 	@Inject(at = @At("TAIL"), method = "render(II)V", locals = LocalCapture.CAPTURE_FAILEXCEPTION)
 	private void commandTips_renderItems(int mouseX, int mouseY, CallbackInfo ci, int i) {
 		List<Suggestion> suggestionList = this.suggestions.getList();
 
 		for(int m = 0; m < i; ++m) {
-			Suggestion suggestion = suggestionList.get(m);
+			Suggestion suggestion = suggestionList.get(m + this.inWindowIndex);
 
 			if (suggestion instanceof ItemRenderableSuggestion) {
 				ItemStack stack = new ItemStack(((ItemRenderableSuggestion) suggestion).getItem());
 				BakedModel model = MinecraftClient.getInstance().getItemRenderer().getHeldItemModel(stack, null, MinecraftClient.getInstance().player);
 				// Scale the item down for screen render then scale it back up
 				Vector3f scale = model.getTransformation().gui.scale;
-				if (stack.getItem() instanceof BlockItem) {
+				if (stack.getItem() instanceof SignItem) {
+					scale.scale(0.55F);
+				} else if (stack.getItem() instanceof BlockItem) {
 					scale.scale(0.65F);
 				} else {
-					scale.scale(0.45F);
+					scale.scale(0.020F);
 				}
 
 				MinecraftClient.getInstance().getItemRenderer().renderGuiItem(stack, this.area.getX() - 16, this.area.getY() + (12 * m) - 1);
 
-				if (stack.getItem() instanceof BlockItem) {
+				if (stack.getItem() instanceof SignItem) { // Special handling for the signs
+					scale.scale((1/0.55F));
+				} else if (stack.getItem() instanceof BlockItem) {
 					scale.scale((1F/0.65F));
 				} else {
-					scale.scale((1F/0.45F));
+					scale.scale((1F/0.020F));
 				}
 			} else if (suggestion instanceof PlayerEntrySuggestion) {
 				PlayerEntrySuggestion playerSuggestion = ((PlayerEntrySuggestion) suggestion);
 				// http://assets.mojang.com/SkinTemplates/4px_reference.png
 				// Reference skin
-				//RenderSystem.pushMatrix();
 				MinecraftClient.getInstance().getTextureManager().bindTexture(playerSuggestion.getSkinTexture());
 				DrawableHelper.blit(this.area.getX() - 16, this.area.getY() + (12 * m) + 2, 8.0F, 8.0F, 8, 8, 64, 64);
-				// TODO - Get all enabled skin parts on the player
+				// We can only do so much if the player instance can't actually be found, so we do our best to get the visible skin parts.
 				PlayerEntity playerEntity = MinecraftClient.getInstance().world.getPlayerByUuid(playerSuggestion.getProfile().getId());
 				if (playerEntity != null) {
 					if (playerEntity.isPartVisible(PlayerModelPart.HAT)) {
 						DrawableHelper.blit(this.area.getX() - 16, this.area.getY() + (12 * m) + 2, 40.0F, 8.0F, 8, 8, 64, 64);
 					}
 				}
-				// We can only do so much if the player instance can't actually be found
-				//RenderSystem.popMatrix();
 			}
 		}
 	}
